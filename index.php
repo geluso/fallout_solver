60467fb690ecbd08b0c66ce842943547b10d773b
Steve Geluso <stevegeluso@gmail.com>
2015-11-18 15:54:49 -0800

    adding writeup for website

---
+++ index.php
@@ -0,0 +1,121 @@
+<?php include "../common_bootstrap.php" ?>
+<?php head("Optimal Fallout Hacking Solver") ?>
+
+<div class="item">
+  <h1>Optimal Fallout Hacking Solver</h1>
+
+  <ol>
+    <li> <a href="#intro">Introduction</a> </li>
+    <li> <a href="#random">Random Guesses</a> </li>
+    <li> <a href="#scoring">Similarity Scoring</a> </li>
+    <li> <a href="#good">Good Choosing</a> </li>
+    <li> <a href="#optimal">Optimal Choosing</a> </li>
+  </ol>
+
+  <a name="intro"></a>
+  <h1>Introduction</h1>
+  <p>
+    Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac
+    consectetur ac, vestibulum at eros. Aenean lacinia bibendum nulla sed consectetur.
+    Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+  </p>
+  <p>
+    Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida
+    at eget metus. Donec sed odio dui. Duis mollis, est non commodo luctus, nisi
+    erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus
+    ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque
+    nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit.
+  </p>
+
+  <a name="random"></a>
+  <h1>Random Guesses</h1>
+  <p>
+    Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac
+    consectetur ac, vestibulum at eros. Aenean lacinia bibendum nulla sed consectetur.
+    Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+  </p>
+  <p>
+    Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida
+    at eget metus. Donec sed odio dui. Duis mollis, est non commodo luctus, nisi
+    erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus
+    ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque
+    nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit.
+  </p>
+  <p>
+    Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida
+    at eget metus. Donec sed odio dui. Duis mollis, est non commodo luctus, nisi
+    erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus
+    ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque
+    nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit.
+  </p>
+
+  <a name="scoring"></a>
+  <h1>Similarity Scoring</h1>
+  <p>
+    Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac
+    consectetur ac, vestibulum at eros. Aenean lacinia bibendum nulla sed consectetur.
+    Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+  </p>
+  <p>
+    Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida
+    at eget metus. Donec sed odio dui. Duis mollis, est non commodo luctus, nisi
+    erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus
+    ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque
+    nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit.
+  </p>
+  <p>
+    Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida
+    at eget metus. Donec sed odio dui. Duis mollis, est non commodo luctus, nisi
+    erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus
+    ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque
+    nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit.
+  </p>
+
+
+  <a name="good"></a>
+  <h1>Good Choosing</h1>
+  <p>
+    Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac
+    consectetur ac, vestibulum at eros. Aenean lacinia bibendum nulla sed consectetur.
+    Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+  </p>
+  <p>
+    Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida
+    at eget metus. Donec sed odio dui. Duis mollis, est non commodo luctus, nisi
+    erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus
+    ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque
+    nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit.
+  </p>
+  <p>
+    Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida
+    at eget metus. Donec sed odio dui. Duis mollis, est non commodo luctus, nisi
+    erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus
+    ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque
+    nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit.
+  </p>
+
+  <a name="optimal"></a>
+  <h1>Optimal Choosing</h1>
+  <p>
+    Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac
+    consectetur ac, vestibulum at eros. Aenean lacinia bibendum nulla sed consectetur.
+    Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+  </p>
+  <p>
+    Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida
+    at eget metus. Donec sed odio dui. Duis mollis, est non commodo luctus, nisi
+    erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus
+    ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque
+    nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit.
+  </p>
+  <p>
+    Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida
+    at eget metus. Donec sed odio dui. Duis mollis, est non commodo luctus, nisi
+    erat porttitor ligula, eget lacinia odio sem nec elit. Cras justo odio, dapibus
+    ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque
+    nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit.
+  </p>
+
+</div>
+
+<?php foot() ?>
